6983ab8828325c75870c3353bdc059f77e6ffce4
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-07 14:59:58 -0300

    up

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '6a500e0e0d2d61350ec1f3faaa7cdb78',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddlare from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddlare);
+
+routes.post('/users', UserController.update);
 
 export default routes;
