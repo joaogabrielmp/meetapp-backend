ef45c157e0e37a8bb1dc9d57dc2b1deb963d8aa2
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-08 13:52:49 -0300

    up

---
+++ src/app/controllers/MeetappController.js
@@ -0,0 +1,19 @@
+import * as Yup from 'yup';
+import Meetapp from '../models/Meetapp';
+
+class MeetappController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+  }
+}
+
+export default new MeetappController();
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,4 @@
 import * as Yup from 'yup';
-
 import User from '../models/User';
 
 class UserController {
